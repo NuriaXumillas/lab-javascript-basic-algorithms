10afc4417624d8b60c96480c2c8f23942795c3f3
Nuria <tu.email@ejemplo.com>
2024-09-26 21:35:19 +0200

    ejercicio dos hecho y tres empezado ni flowers

--- index.js
+++ index.js
@@ -12,5 +12,34 @@ console.log(`The navigator's name is  ${hacker2}`)
 // Iteration 2: Conditionals
 
 
+function compareNamesLong(user1, user2) {
 
-// Iteration 3: Loops
+    if (user1.length > user2.length) {
+        console.log (`The driver has the longest name, it has ${user1.length} characters.`)
+
+    } else if (user2.length > user1.length) {
+        console.log (`It seems that the navigator has the longest name, it has ${user2.length} characters.`)
+    
+    } else {
+        console.log(`Wow, you both have equally long names, ${user1} characters!.`)
+    }
+}
+
+compareNamesLong(hacker1, hacker2)
+
+// Iteration 3: Loops 
+
+function processNames(driver, navigator) {
+    const driverUpperCase = ''
+
+    for (let i = 0; i < driver.length; i++) {
+
+        driverUpperCase += drive[i].toUpperCase()
+        
+        if (i < driverName.length - 1) {
+            driverUpperCase += ' '
+        }
+    }
+}
+
+console.log(driverUpperCase);
