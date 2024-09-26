35a9cf8331307147871d66bfc46dc59bd53924fe
Nuria <tu.email@ejemplo.com>
2024-09-26 21:00:35 +0200

    primer ejercicio

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // Iteration 1: Names and Input
 
 
+
+const hacker1 = "Nuria"
+const hacker2 = "Carlos"
+
+console.log(`The driver's name is ${hacker1}`)
+console.log(`The navigator's name is  ${hacker2}`)
+
+
 // Iteration 2: Conditionals
+
 
 
 // Iteration 3: Loops
